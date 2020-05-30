bb95c19c9de0f9b12b50f23e26065afd4b075342
matsu <50922910+mtsml@users.noreply.github.com>
2020-05-30 14:14:47 +0900

    [Add] フィールドを追加

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
 import ButtonArea from './ButtonArea.js'
+import Contents from './Contents.js'
+import Hand from './Hand.js'
 
-function App() {
+const App = () => {
+  const players = [
+    {name: "hoge", hand: 1},
+    {name: "fuga", hand: 2},
+    {name: "foo", hand: 3},
+    {name: "bar", hand: 4},
+    {name: "baz", hand: 5}
+  ]
   return (
     <div className="App">
-      <ButtonArea />
+      <Contents title='フィールド'>
+        {players.map(player => {
+          return (
+            <Hand text={player.name} />
+          )
+        })}
+      </Contents>
+      <Contents title='得点カード'>
+
+      </Contents>
+      <Contents title='手札'>
+        <ButtonArea />
+      </Contents>
     </div>
   );
 }
---
+++ src/Contents.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const Contents = (props) => {
+return (
+        <Card>
+            <Card.Header>{props.title}</Card.Header>
+            <Card.Body>
+                {props.children}
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default Contents

e93b41ecbdc427da078142db1114b0d5d1df15a0
matsu <50922910+mtsml@users.noreply.github.com>
2020-05-30 12:20:20 +0900

    [Add] ボタン追加

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import ButtonArea from './ButtonArea.js'
 
 function App() {
   return (
     <div className="App">
-      hagetaka
+      <ButtonArea />
     </div>
   );
 }

d12418f68a4328783a9516d6f719039ef8c2bab4
matsu <50922910+mtsml@users.noreply.github.com>
2023-09-18 02:08:46 +0900

    Updates to Client Rendering APIs
    https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from './store/index'
 import Router from './container/Router.js';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './css/index.scss';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root'));
+root.render(
     <Provider>
         <Router />
-    </Provider>,
-    document.getElementById('root')
+    </Provider>
 );
